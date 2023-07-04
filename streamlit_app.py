f4e5b1f457ed87051e8b74607bbcc01b784e083b
rezashihran <79193722+rezashihran@users.noreply.github.com>
2023-07-04 23:48:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,12 +16,8 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 # Here's your DataFrame
-df_t = pd.DataFrame(column_items.csv
-    ...  # replace with your actual DataFrame
-)
-rules = pd.DataFrame(rules_items.csv
-    ...  # replace with your actual rules DataFrame
-)
+df_t = pd.DataFrame(column_items.csv)
+rules = pd.DataFrame(rules_items.csv)
 
 column_names = df_t.columns.tolist()
 
